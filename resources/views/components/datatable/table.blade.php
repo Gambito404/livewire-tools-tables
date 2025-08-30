bc3f0f70aa11f5bc022d21e818ba5e0f9833ed97
Gambito404 <gabrielberriosmendoza@gmail.com>
2025-08-30 15:58:05 -0400

    Update table.blade.php

--- resources/views/components/datatable/table.blade.php
+++ resources/views/components/datatable/table.blade.php
@@ -1,27 +1,23 @@
-<table class="tools-table">
+<table>
     <thead>
         <tr>
             @foreach($columns as $column)
-                <th wire:click="sortBy('{{ $column['field'] }}')">
-                    <span>{{ $column['label'] }}</span>
-                    @if($sortColumn === $column['field'])
-                        <span>{{ $sortDirection === 'asc' ? '↑' : '↓' }}</span>
-                    @endif
-                </th>
+                @unless($column->hidden)
+                    <th>{{ $column->label }}</th>
+                @endunless
             @endforeach
         </tr>
     </thead>
     <tbody>
-        @forelse($processedRows as $row)
+        @foreach($rows as $index => $row)
             <tr>
-                                @foreach($row->cells as $cell)
-                    <td>{!! $cell !!}</td>
+                @foreach($columns as $column)
+                    @unless($column->hidden)
+                        <td>{{ $column->getValue($row, $index) }}</td>
+                    @endunless
                 @endforeach
             </tr>
-        @empty
-            <tr>
-                <td colspan="{{ count($columns) }}">No results found.</td>
-            </tr>
-        @endforelse
+        @endforeach
     </tbody>
 </table>
+
